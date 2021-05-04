7de69b5e9f63c918276d2e05aafd91b8da4a15b3
1gni5 <1gni5@net-c.com>
2021-05-04 17:00:38 +0200

    Add __init__() method

--- src/polygon.py
+++ src/polygon.py
@@ -2,3 +2,8 @@
 
 class Polygon():
     '''Représente un polygône en 2 dimensions'''
+
+    def __init__(self, vertices):
+        '''Créer un polygône à partir d'une liste de namedtuple.'''
+
+        self.vertices = vertices
