6a0648d6e3f959e473a1e45dbac4ca49c47a3b28
1gni5 <1gni5@net-c.com>
2021-05-04 16:19:58 +0200

    Add test class skeleton

---
+++ tests/polygon_test.py
@@ -0,0 +1,14 @@
+# polygon_test.py
+
+from sys import path
+from unittest import TestCase
+
+# Ajoute le chemin vers les sources
+path.append('src')
+
+from polygon import Polygon
+
+class TestPolygon(TestCase):
+    '''Test les fonctionnalité de la classe Polygon.'''
+
+    # TODO: Ajouter des tests
