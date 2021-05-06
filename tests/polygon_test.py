b18eb62bcbdd6022a41dfa6626e0d403bb439e81
1gni5 <1gni5@net-c.com>
2021-05-06 11:18:03 +0200

    Change tests to check __init__ with less than 3 vertices

--- tests/polygon_test.py
+++ tests/polygon_test.py
@@ -17,6 +17,25 @@ class TestPolygon(TestCase):
 
         self.Vertex = namedtuple('Vertex', ['x', 'y'])
 
+    def test_polygon_init_with_less_than_3_vertices(self):
+        '''Essaye de créer un polygon avec moins de 3 sommets.'''
+
+        # Créer une liste de points
+        vertices = list(map(
+            lambda coords : self.Vertex(*coords),
+            [[-1, 1], [-1, -1], [1, -1], [1, 1]]
+        ))
+        
+        # Un polygône doit avoir au moins 3 sommet
+        with self.assertRaises(ValueError):
+            Polygon([])
+            
+        with self.assertRaises(ValueError):
+            Polygon(vertices[:1])
+
+        with self.assertRaises(ValueError):
+            Polygon(vertices[:2])
+        
     def test_polygon_area_with_normal_values(self):
         '''Test le fonctionnement normal de la propriété area().'''
 
@@ -28,13 +47,6 @@ class TestPolygon(TestCase):
 
         # Vérifie que la valeur match le jeux de test
         self.assertEqual(Polygon(vertices).area, 4)
-
-    def test_polygon_area_with_no_values(self):
-        '''Test le fonctionnement de la propriété area() avec un Polygon
-        vide.'''
-
-        # Une figure sans sommets n'a pas d'air
-        self.assertEqual(Polygon([]).area, 0)
 
     def test_polygon_gravity_with_normal_values(self):
         '''Test le fonctionnement normal de la propriété gravity().'''
