2c0141ade79ad68142197bf434ae5a6f0b5b2baf
1gni5 <1gni5@net-c.com>
2021-05-07 17:23:59 +0200

    Add bulk test against test_set

--- tests/polygon_test.py
+++ tests/polygon_test.py
@@ -94,4 +94,76 @@ class TestPolygon(TestCase):
         for a,b,s in zip(polygon.vertices, neighbors, polygon.segments):
             self.assertEqual((a,b), s)
 
+    def test_polygon_all(self):
+        """Vérifie que toutes les méthodes public fonctionne avec toute les
+        valeurs du jeu de test.  
+        """
+
+        TestSet = namedtuple('TestSet', ['vertices', 'area', 'gravity', 'cow'])
+
+        # Jeu de test
+        test_sets = [
+            TestSet(
+                vertices = [[-1, 1], [-1, -1], [1, -1], [1, 1]],
+                area = 4,
+                gravity = self.Vertex(0, 0),
+                cow = True,
+            ),
+            TestSet(
+                vertices =  [
+                    [-16.6, -20.1], [-12.6, -18.6],
+                    [-11.6, -16.6], [-15.1, -15.1]
+                ],
+                area = 13.125,
+                gravity = self.Vertex(-14.226, -17.555),
+                cow = True,
+            ),
+            TestSet(
+                vertices = [
+                    [-1.1, -1.5], [2.1, 3.012],
+                    [5.6, -1.21], [1.97, 4.07]
+                ],
+                area = 3.563,
+                gravity = self.Vertex(1.978, 1.903),
+                cow = False,
+            ),
+        ]
+
+        # Test pour chaque jeu de données
+        for test_set in test_sets:
+            
+            # Créer une liste de points
+            vertices = list(map(
+                lambda coords : self.Vertex(*coords),
+                test_set.vertices,
+            ))
+
+            # Créer le polygon de test
+            polygon = Polygon(vertices)
+
+            # Comparaison des aires
+            self.assertAlmostEqual(
+                polygon.area,
+                test_set.area,
+                3,
+            )
+
+            # Comparaison des centres de gravité
+            self.assertAlmostEqual(
+                polygon.gravity.x,
+                test_set.gravity.x,
+                2,
+            )
+                        # Comparaison des centres de gravité
+            self.assertAlmostEqual(
+                polygon.gravity.y,
+                test_set.gravity.y,
+                2,
+            )
+
+            # Comparaison des positions de la vache
+            self.assertEqual(
+                polygon.gravity in polygon,
+                test_set.cow
+            )
         
