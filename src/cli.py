118b77e20bdc8412ea127782f425ba549d3c6d3d
1gni5 <1gni5@net-c.com>
2021-05-14 17:53:06 +0200

    Refactor some code

--- src/cli.py
+++ src/cli.py
@@ -1,8 +1,9 @@
 # cli.py
 
 from sys import argv
-from dataload import dataload
+
 from polygon import Polygon
+from dataload import load_from_csv
 
 def main():
 
@@ -14,14 +15,12 @@ def main():
             print(f'{filename}', end=": ")
 
         # Créer l'enclos et la vache
-        pen = Polygon(dataload(filename))
+        pen = Polygon(load_from_csv(filename))
         cow = pen.gravity
 
         # Affiche la réponse
-        if cow in pen:
-            print("La vache est à l'intérieur de l'enclos.")
-        else:
-            print("La vache est à l'extérieur de l'enclos.")
+        answer = 'intérieur' if cow in pen else 'extérieur'
+        print(f"La vache est à l'{answer} de l'enclos.")
 
     # L'utilisateur n'a pas fourni de fichier
     if len(argv) <= 1:
