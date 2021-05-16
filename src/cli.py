cb452fea03baa355e0cf65adbeadeda6de3ad29a
1gni5 <1gni5@net-c.com>
2021-05-16 10:17:51 +0200

    Add flag --graph

--- src/cli.py
+++ src/cli.py
@@ -1,15 +1,36 @@
 # cli.py
 
 from sys import argv
+from matplotlib import pyplot
 
 from polygon import Polygon
 from dataload import load_from_csv
 
 
+def draw(pen, cow):
+    """Dessine l'enclot ainsi que la vache."""
+
+    # Dessine l'enclot
+    for a, b in pen.segments:
+        pyplot.plot(a, b, marker='o')
+
+    # Dessine la vache
+    pyplot.scatter(*cow)
+    pyplot.show()
+
 def main():
 
+    # Filtre les arguments et les options
+    options = list(
+        filter(lambda x : x.startswith('--'), argv[1:])
+    )
+
+    arguments = list(
+        filter(lambda x : x not in options, argv[1:])
+    )
+
     # Pour chaque fichier passé en paramètre
-    for filename in argv[1:]:
+    for filename in arguments:
 
         # Si l'utilisateur fourni plusieurs fichiers sources
         if len(argv[1:]) > 1:
@@ -22,6 +43,10 @@ def main():
         # Affiche la réponse
         answer = 'intérieur' if cow in pen else 'extérieur'
         print(f"La vache est à l'{answer} de l'enclos.")
+
+        # Affiche le graphique
+        if '--graph' in options:
+            draw(pen, cow)
 
     # L'utilisateur n'a pas fourni de fichier
     if len(argv) <= 1:
